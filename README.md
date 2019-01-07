940fc33c6d163489cb5d30cec15d4206214388df
Jordan <ouyangjordan@gmail.com>
2019-01-06 19:33:22 -0800

    another commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 SLC-Bot
+
+Link to the worksheet
+
+https://docs.google.com/document/d/1XGntCNaxR00Ywk8f2XPgx8r5wihIu5QYHHS5bIRatE8/edit
+
+
+This app will be used to demonstrate how you can interact with the twitter API. Workshop participants will be able to use the keys to experiment with the twitter platform.
---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  consumer_key: '4TPoBQlH83MMaPnffiDW8ut5k',
+  consumer_secret: 'kGyEsrxRg62y9na1M7lYXrtYL9I4uUK9cccMTOg0cnf0KeenJe',
+  access_token_key: '1005211976241328128-QL6PX7EHxXp04Rhh8VrkfGbkRtWtri',
+  access_token_secret: 'YmWoOYrUUXijC1AAH9bAZo2poLym1oc9p83OPcwo9c7jr'
+}
