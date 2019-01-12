9e428a70c0f4b41d71128eb79238dd21cc8ad948
Jordan <ouyangjordan@gmail.com>
2019-01-12 14:00:16 -0800

    yep

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
 
-
-T.post('statuses/update', {status: "Whats up world pt 3"},  function(error, tweet, response) {
-  if(error) {
-    console.log(error);
-    throw error;
-  }
+T.post('statuses/update', { status: 'hello world, but again.' }, function(err, data, response)
+{
 })
--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 //Create your twitter app, go to keys and tokens,
 //generate them, copy and paste
 module.exports = {
-  consumer_key: 'jRTfSsrDDuDna0eoU5vZV0lvO',
-  consumer_secret: 'wMMllpWkr3ZOWl5cF26G2VuyVS6FikEZ5nGTuECMhLWhKX42wi',
-  access_token_key: '1078479620813385728-JFbCio7jM9xj3OMcZ871SGjV3YB3A0',
-  access_token_secret: '4QL6uKlJL5r10xohsqqeoS9BC6C1emWvkVfdV2oHPZAYR'
+  consumer_key: '4AO13gnPJikCDtvgRCGnOMTqA',
+  consumer_secret: 'cKlEE4v7iKBm9aOq0vRzNSXSIdxoKVt8oKLKE3oN5QKCtQeIcC',
+  access_token_key: '1078479620813385728-blKqf7pNNWhIST2zJBjhdwsgXQ18ig',
+  access_token_secret: '44rzzglUPu21LGkW0L7gOr3pxnmZEcV6Ezr53Hje8Tolq'
 }
